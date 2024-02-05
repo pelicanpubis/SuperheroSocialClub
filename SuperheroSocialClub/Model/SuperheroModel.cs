7641a545cc9361f522e014183e5d8774af3013cb
pelicanpubis <norah.binsalamah@hotmail.com>
2024-02-05 15:56:40 +0100

    Jag har lagt till den nu...lol

--- SuperheroSocialClub/Model/SuperheroModel.cs
+++ SuperheroSocialClub/Model/SuperheroModel.cs
@@ -2,5 +2,14 @@
 {
     public class SuperheroModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SecretIdentity { get; set; }
+
+        public string Superpower { get; set; }
+
+        public string Image { get; set; }   
     }
 }
