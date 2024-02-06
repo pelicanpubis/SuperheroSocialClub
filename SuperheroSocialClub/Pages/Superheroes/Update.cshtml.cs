2c30eb298dc816eaa7d9b7928d17a3af8e275a20
Adirsflo <rida.abdal@gmail.com>
2024-02-06 17:51:40 +0100

    Update page & added method in SuperheroesRepo-commit

--- SuperheroSocialClub/Pages/Superheroes/Update.cshtml.cs
+++ SuperheroSocialClub/Pages/Superheroes/Update.cshtml.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperheroSocialClub.Database;
+using SuperheroSocialClub.Model;
 
 namespace SuperheroSocialClub.Pages.Superheroes
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        public SuperheroModel? Superhero { get; set; }
+
+        public void OnGet(int id)
         {
+            Superhero = SuperheroesRepo.Superheroes.FirstOrDefault(s => s.Id == id);
+        }
+        public void OnPost(int id)
+        {
+            Superhero = SuperheroesRepo.Superheroes.FirstOrDefault(s => s.Id == id);
+
+            if (Superhero != null && ModelState.IsValid)
+            {
+                SuperheroesRepo.UpdateSuperhero(Superhero);
+                TempData["Message"] = "Superhero updated successfully!";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Update failed. Please check your inputs.";
+            }
         }
     }
 }
