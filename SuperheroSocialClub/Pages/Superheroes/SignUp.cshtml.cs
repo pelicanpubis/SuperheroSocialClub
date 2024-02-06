9190375fc41c80a0605874641c4529b7b030cded
pelicanpubis <norah.binsalamah@hotmail.com>
2024-02-06 18:59:00 +0100

    DET GÅR ÄNTLIGEN ATT LÄGGA TILL SUPERHEROES :D

--- SuperheroSocialClub/Pages/Superheroes/SignUp.cshtml.cs
+++ SuperheroSocialClub/Pages/Superheroes/SignUp.cshtml.cs
@@ -11,7 +11,9 @@ namespace SuperheroSocialClub.Pages.Superheroes
         public List<SuperheroModel> Superheroes { get; set; }
 
 
+        [BindProperty] 
 
+        public int Id { get; set; } 
         //Borde göra de till 
         [BindProperty]
         public string? Name { get; set; } 
@@ -28,64 +30,42 @@ namespace SuperheroSocialClub.Pages.Superheroes
 
 
 
-        public IActionResult OnPostCreate()
-        {
 
+        public IActionResult OnPost()
+        {
             if (Name != null && SecretIdentity != null && Superpower != null && Image != null)
             {
-                // Skapa ett unikt id för den nya superhjälten baserat på antalet befintliga superhjältar
-                int newId = SuperheroesRepo.Superheroes.Count + 1;
 
-                // Skapa en ny instans av SuperheroModel med alla nödvändiga parametrar
                 SuperheroModel newSuperhero = new SuperheroModel(
-                    newId,
-                    Name,
-                    SecretIdentity,
-                    Superpower,
-                    Image
-                );
+                   Id,
+                     Name,
+                  SecretIdentity,
+                   Superpower,
+                   Image
+                      );
 
-                // Lägg till den nya superhjälten i listan
+
+                //SuperheroModel newSuperhero = new SuperheroModel()
+                //(
+                //   //Id = Superheroes.Count +1 ,
+                //   //Name = Name,
+                //   //SecretIdentity = SecretIdentity,
+                //   //Superpower = Superpower,
+                //   //Image = Image
+
+
+                //);
+
                 SuperheroesRepo.Superheroes.Add(newSuperhero);
 
-                return RedirectToPage("/Superheroes");
+                return RedirectToPage("/Superheroes/Index");
             }
             else
             {
                 return Page();
             }
-
-
-            //if (Name != null && SecretIdentity != null && Superpower != null && Image != null)
-            //{
-            //    SuperheroModel newSuperhero = new SuperheroModel
-            //    (
-
-
-
-            //    Name = Name,
-            //    SecretIdentity = SecretIdentity,
-            //    Superpower = Superpower,
-            //    Image = Image
-
-
-
-            //    );
-
-            //    SuperheroesRepo.Superheroes.Add(newSuperhero);
-
-            //    return RedirectToPage("/Superheroes");
-            //}
-
-            //else
-            //{
-            //    return Page();
-
-            //}
-
-
-
         }
+
         public void OnGet()
         {
         }
